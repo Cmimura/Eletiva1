6a539c9feb9e31b2d67704ef7d5390c49a808c23
caua <cauamimura@gmail.com>
2026-09-17 21:21:39 -0300

    Realização da Lista 2

---
+++ Lista2/ex2.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 2</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 2</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="num1" class="form-label">Digite o primeiro número</label>
+              <input type="number" id="num1" name="num1" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="num2" class="form-label">Digite o segundo número</label>
+              <input type="number" id="num2" name="num2" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    $num1 = $_POST["num1"] ?? 0;
+    $num2 = $_POST["num2"] ?? 0;
+    if ($num1 == $num2)
+        {
+            echo "<p>Números iguais: $num1</p>";
+        }
+    elseif ($num1 < $num2)
+        {
+            echo "<p>$num1 $num2</p>";
+        }
+    else
+    {
+        echo "<p>$num2 $num1</p>";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
---
+++ Lista2/ex7.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercício 7</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercício 7</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="num1" class="form-label">Digite um número</label>
+              <input type="number" id="num1" name="num1" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    $num1 = $_POST["num1"] ?? 0;
+    $i = $num1;
+    while ($i > 0) { 
+        echo $i;
+        $i -= 1;
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
